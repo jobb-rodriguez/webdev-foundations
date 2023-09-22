8c423a079bba558199bab5584a8c87530a646b7d
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-22 19:20:32 +0800

    Add: DOM

---
+++ javascript/dom.js
@@ -0,0 +1,45 @@
+// DOM Selectors
+// console.log("Tag:", document.getElementsByTagName("h1"));
+// console.log("Id:", document.getElementById("first").textContent);
+// console.log("Class:", document.getElementsByClassName("italic"));
+
+
+// console.log("Query Selector:", document.querySelector("#first"));
+// console.log("First .italic:", document.querySelector(".italic"));
+// console.log("All .italic:", document.querySelectorAll(".italic"));
+// console.log("All <li>:", document.querySelectorAll("li"));
+
+// document.querySelector("p").getAttribute('id');
+// document.querySelector("p").setAttribute('class', 'italic');
+// console.log("Query Selector All:", document.querySelectorAll(".italic"));
+
+// document.querySelector("body").style.background = "#00308F";
+
+// console.log("Class List of first .italic:", document.querySelector(".italic").classList);
+// document.querySelector(".italic").classList.add("bold");
+// console.log("Class List of first .italic:", document.querySelector(".italic").classList);
+
+// document.querySelectorAll(".italic")[1].classList.remove("italic");
+
+// console.log(document.querySelectorAll("li")[0].parentElement);
+// console.log(document.querySelector("ul").children[0]);
+
+// DOM Events
+const shoppingList = ["Eggs", "Bueno", "Coke", "Mayonnaise"]
+
+const addItemButton = document.getElementById("addItem");
+const masterList = document.querySelector("ul");
+addItemButton.addEventListener("click", () => {
+    if(shoppingList.length === 1) {
+        addItemButton.setAttribute('disabled', 'true');
+        addItemButton.textContent = "No more item to add";
+    }
+    
+    const li = document.createElement("li");
+    // first item in the array
+    const item = shoppingList[0];
+    shoppingList.shift();
+    li.appendChild(document.createTextNode(item));
+
+    masterList.appendChild(li);
+});
