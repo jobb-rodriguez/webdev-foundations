e4e2ae708fd8cdbaf4d9b08b29ec62cfb8355294
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-29 10:23:12 +0800

    Update: DOM Recording

--- javascript/js/dom.js
+++ javascript/js/dom.js
@@ -9,10 +9,11 @@
 // console.log("All .italic:", document.querySelectorAll(".italic"));
 // console.log("All <li>:", document.querySelectorAll("li"));
 
-// document.querySelector("p").getAttribute('id');
+// console.log(document.querySelector("p").getAttribute('id'));
 // document.querySelector("p").setAttribute('class', 'italic');
 // console.log("Query Selector All:", document.querySelectorAll(".italic"));
 
+// // querySelector.style.property = value
 // document.querySelector("body").style.background = "#00308F";
 
 // console.log("Class List of first .italic:", document.querySelector(".italic").classList);
@@ -29,12 +30,24 @@ const shoppingList = ["Eggs", "Bueno", "Coke", "Mayonnaise"]
 
 const addItemButton = document.getElementById("addItem");
 const masterList = document.querySelector("ul");
+/* 
+selector.addEventListener("event", () => {
+    action
+})
+*/
 addItemButton.addEventListener("click", () => {
     if(shoppingList.length === 1) {
         addItemButton.setAttribute('disabled', 'true');
         addItemButton.textContent = "No more item to add";
     }
-    
+    /*
+        <ul>
+            ...
+            <li>Eggs</li>
+        </ul>
+        item = "Eggs"
+        shoppingList = ["Bueno", "Coke", "Mayonnaise"]
+    */
     const li = document.createElement("li");
     // first item in the array
     const item = shoppingList[0];
