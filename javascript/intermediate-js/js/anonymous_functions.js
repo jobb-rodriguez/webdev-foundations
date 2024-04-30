f89f090a0188f6f52c4e212a364e80ce897de873
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2024-04-30 10:07:06 +0800

    Feature: Anonymous Functions

---
+++ javascript/intermediate-js/js/anonymous_functions.js
@@ -0,0 +1,13 @@
+// Unnamed Function
+let greet = function (platform) { 
+    console.log("Welcome to ", platform); 
+}; 
+  
+greet("America!"); 
+
+// Arrow Function
+let greetArrow = (platform) => {
+    console.log("Welcome to ", platform); 
+}
+  
+greet("Philippines!"); 
