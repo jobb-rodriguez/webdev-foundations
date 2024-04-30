0f74bbcc95291b75865cdf0df1e71dc544661aa1
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2024-04-30 10:56:29 +0800

    Update: Recorded version of data_types.js

--- javascript/intermediate-js/js/data_types.js
+++ javascript/intermediate-js/js/data_types.js
@@ -2,12 +2,13 @@
 // const let you set constant variables
 // constant variables have constant values
 const isTrue = true;
+// isTrue = false; // this will return an error since you cannot update the values of constant variables
 
 // let
 // def: The let declaration declares re-assignable, block-scoped local variables, optionally initializing each to a value.
 let x = 1;
-// let x = 2; -> causes an error (let declarations cannot be redeclared by any other declaration in the same scope.)
-x = 3; // reassign the variable instead of redeclaring
+// let x = 2; // -> causes an error (let declarations cannot be redeclared by any other declaration in the same scope.)
+// x = 3; // reassign the variable instead of redeclaring
 
 if (x === 1) {
   let x = 2;
@@ -22,6 +23,14 @@ console.log(x);
 // var
 var y = 1;
 var y = 2; // unlike in let, redeclaring does not cause an error
+
+if (y === 2) {
+  var y = 3;
+
+  console.log(y);
+}
+
+console.log(y);
 
 /* NOTES
 1. Use const when you can, and use let when you have to.
