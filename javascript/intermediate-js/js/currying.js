63f988268df547564e540afdb9701dba6271e483
Jobb <76737172+jobb-rodriguez@users.noreply.github.com>
2024-05-04 16:07:52 +0800

    Update: currying.js - examples provided by Jio

--- javascript/intermediate-js/js/currying.js
+++ javascript/intermediate-js/js/currying.js
@@ -27,10 +27,28 @@ console.log(get_func_inner(3));
 
 // CURRYING
 function calculateVolumeCurrying(length) {
+    length *= 2;
     return (breadth) => { // remembers length
         return (height) => { // remembers length and breadth
             return length * breadth * height;
         }
     }
 }
+
+let lengthwith4 = calculateVolumeCurrying(4)
+let breadthwith5 = lengthwith4(5)
+
+console.log("test 1", lengthwith4(5)(6))
+console.log("test 2", breadthwith5(6))
+
 console.log(calculateVolumeCurrying(4)(5)(6));
+
+function max_2 (left, right) {
+    return left > right ? left : right
+}
+
+function max_3 (left, mid, right) {
+    return max_2(left, max_2(mid, right))
+}
+
+console.log(max_3(10, 7, 3))
